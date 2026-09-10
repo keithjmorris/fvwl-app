d53240823c1e11fc613969a7b9159a0bf8b700c0
keithjmorris <keith.morris@outlook.com>
2026-09-10 17:28:07 +0100

    Correct spelling of AuthGuard

--- components/Authguard.jsx
+++ components/Authguard.jsx
@@ -3,7 +3,7 @@ import { useAuth } from '@/lib/AuthContext';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
-export default function AuthGuard({ children }) {
+export default function AuthGuard({ children })  {
   const { user, loading } = useAuth();
   const router = useRouter();
 
